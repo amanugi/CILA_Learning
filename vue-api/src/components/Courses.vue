55d545915f330f8f121ab8a146d8bc960edcb1f2
Aman Singh <amans271999@gmail.com>
2021-01-19 20:01:33 +0530

    vueApi

---
+++ vue-api/src/components/Courses.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Coding Ninjas Courses</h1>
+        <div>
+            <ul>
+                <li v-for="course in listofCourses" :key="course.id">{{course.title}}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import Vue from 'vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+Vue.use(VueAxios, axios);
+
+export default {
+    name: 'Courses',
+    data() {
+        return {
+            listofCourses: []
+        }
+    },
+    mounted() {
+        Vue.axios.get('https://api.codingninjas.com/api/v3/courses').then(res => {
+            this.listofCourses = res.data.data.courses;
+            console.warn(res);
+        })
+    }
+}
+</script>
+
+<style>
+    
+</style>
