a4abd030b60a11d797b3ea606b701e473bb9254b
Aman Singh <amans271999@gmail.com>
2021-01-26 18:17:32 +0530

    Learning quasar

---
+++ quasar/src/layouts/MainLayout.vue
@@ -0,0 +1,121 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        
+        <q-toolbar-title>
+          <q-img src="../statics/logo.png" alt="logo" style="width: 60px"> </q-img> 
+        </q-toolbar-title>
+
+        <div class="gt-sm">
+          <q-btn 
+          outline
+          squared
+          label="Courses"
+          class="q-mr-sm"
+          >
+          </q-btn>
+
+          <q-btn 
+          outline
+          rounded
+          label="Sign up"
+          class="q-mr-sm"
+          >
+          </q-btn>
+          
+          <q-btn 
+          outline
+          rounded
+          label="Sign in"
+          >
+          </q-btn>
+        </div>
+
+        <div class="lt-md">
+          <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          />
+        </div>
+        
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          <!-- Essential Links -->
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+          style="color:green"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer elevated style="z-index: -1">
+        <q-toolbar>
+          <q-space />
+            Copyright &copy; 2021
+          <q-space />
+        </q-toolbar>
+      </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Sign Up',
+    caption: '',
+    icon: '',
+    link: ''
+  },
+  {
+    title: 'Sign In',
+    caption: '',
+    icon: '',
+    link: ''
+  },
+  {
+    title: 'Courses',
+    caption: '',
+    icon: '',
+    link: ''
+  },
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ quasar/src/pages/Index.vue
@@ -0,0 +1,120 @@
+<template>
+  <q-page class="bg-deep-purple-1">
+    <div class="row bg-primary">
+      <div class="col">
+        <div style="text-align:center; max-width:650px; margin-left:auto; margin-right:auto">
+          <div class="text-h4 text-bold text-white q-mt-xl q-pl-lg q-pb-lg">Learn Quasar framework from Industry Experts</div>
+          <div class="text-white text-h6 q-pl-lg q-pr-lg q-pb-sm"> Join our community today to get started</div>
+          <q-input
+            color="black"
+            bg-color="white"
+            filled
+            placeholder="Your Email"
+            class="q-pl-lg q-pr-lg q-pb-lg"
+        >
+          <template v-slot:prepend>
+            <q-icon name="email" />
+          </template>
+          <template v-slot:append>
+            <q-btn color="purple">Start Learning</q-btn>
+          </template>
+          </q-input>
+        </div>
+      </div>
+      <div class="col gt-sm">
+        <q-img src="../statics/imagee.jpg" alt="img" class="q-mx-sm q-my-sm"></q-img>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="text-h5 text-bold q-px-md q-pt-sm">Latest Chapters</div>
+    </div>
+    <div class="row">
+      <div class="col-12 col-md">
+        <div class="q-pa-md">
+          <q-card class="my-card">
+          <q-video src="https://www.youtube.com/embed/BK66mQTSl7U" />
+
+          <q-card-section>
+            <div class="text-h6">Quasar Basics</div>
+            <div class="text-subtitle2">by Codeguru</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </q-card-section>
+          </q-card>
+        </div>
+      </div>
+      <div class="col-12 col-md">
+        <div class="q-pa-md">
+          <q-card class="my-card">
+          <q-video src="https://www.youtube.com/embed/As5lzor2NQ8" />
+
+          <q-card-section>
+            <div class="text-h6">Quasar setup</div>
+            <div class="text-subtitle2">by Codeguru</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </q-card-section>
+          </q-card>
+        </div>
+      </div>
+      <div class="col-12 col-md">
+        <div class="q-pa-md">
+          <q-card class="my-card">
+          <q-video src="https://www.youtube.com/embed/GV-D85D9KJQ" />
+
+          <q-card-section>
+            <div class="text-h6">Qusar-Todo</div>
+            <div class="text-subtitle2">by Danny Connell</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </q-card-section>
+          </q-card>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="text-h5 text-bold q-px-md q-pt-sm">Featured Lessons</div>
+    </div>
+  <div class="row">
+    <div class="col-12 col-md">
+      <div class="q-pa-md">
+        <q-card class="my-card">
+          <div class="row">
+            <div class="col-12 col-md-4 q-pa-sm">
+              <q-img  src="../statics/image.jpeg"></q-img>
+            </div>
+            <div class="col-12 col-md-8">
+              <div style="color:grey" class="text-h5 text-bold q-pa-sm">Getting started with Quasar framework</div>
+              <div class="text-h6 text-italic q-pa-sm">Using Quasar framework we can create multi-platform-websites 
+                such as :
+                <ul>
+                  <li>Web</li>
+                  <li>Desktop</li>
+                  <li>Mobile</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </q-card>
+      </div>
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
