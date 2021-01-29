388b847c88371bc6310a73eb56ca5915c76d73b4
Aman Singh <amans271999@gmail.com>
2021-01-29 21:10:52 +0530

    Auth page, Chat Page, Users Page

---
+++ chat-app/src/pages/UsersPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-page class="flex q-pa-md">
+    <q-list 
+      class="full-width"
+      separator
+      clickable
+      v-ripple
+    >
+      <q-item 
+        v-for="user in users" 
+        :key="user.id"
+        to="/chat"
+      >
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge 
+            :color="user.online ? 'light-green-5' : 'grey-5' ">
+            {{ user.online ? 'online' : 'offline' }}
+          </q-badge>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        users: [ {
+            id: 1,
+            name: 'Aman',
+            online: true
+          }, 
+          {
+            id: 2,
+            name: 'Parikh',
+            online: false
+          }, 
+          {
+            id: 3,
+            name: 'Ankush',
+            online: true
+          }, 
+        ]
+      }
+    }
+  }
+</script>
